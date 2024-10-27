521026c394cf5c1a944f23d889f258356cba2ee9
sparkie11 <gautamwise.s@gmail.com>
2024-10-27 21:54:44 +0530

    n

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import Testimonials from "./components/testimonials/Testimonials";
 import Topbar from "./components/topbar/Topbar";
 import Work from "./components/works/Work";
 import "./app.scss";
+import { useState } from "react";
+import Menu from "./components/menu/Menu";
 
 function App() {
+  const [menuOpen,setMenuOpen]=useState(false)
   return (
     <div className="app">
-      <Topbar/>
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className="sections">
         <Intro/>
         <Portfolio/>
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,9 @@
 import { Mail, Person } from '@mui/icons-material'
 import './topbar.scss'
 
-export default function Topbar() {
+export default function Topbar({menuOpen, setMenuOpen}) {
   return (
-    <div className="topbar">
+    <div className={"topbar " + (menuOpen &&  "active")}>
       <div className="wrapper">
         <div className="left">
             <a href="#intro" className='logo'>userLogo</a>
@@ -17,7 +17,7 @@ export default function Topbar() {
             </div>
         </div>
         <div className="right">
-            <div className="hamburger">
+            <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
                 <span className="line1"></span>
                 <span className="line2"></span>
                 <span className="line3"></span>
