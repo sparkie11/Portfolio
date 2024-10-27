4b51a5a5cf15f6057ae084ebb444a4c5efb5301e
sparkie11 <gautamwise.s@gmail.com>
2024-10-27 18:59:07 +0530

    n

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
+import Contact from "./components/contact/Contact";
+import Intro from "./components/intro/Intro";
+import Portfolio from "./components/portfolio/Portfolio";
+import Testimonials from "./components/testimonials/Testimonials";
+import Topbar from "./components/topbar/Topbar";
+import Work from "./components/works/Work";
+import "./app.scss";
 
 function App() {
   return (
-    <div className="App">
-ss
+    <div className="app">
+      <Topbar/>
+      <div className="sections">
+        <Intro/>
+        <Portfolio/>
+        <Work/>
+        <Testimonials/>
+        <Contact/>
+        
+      </div>
+
     </div>
   );
 }
