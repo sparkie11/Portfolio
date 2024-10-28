6abd47d52ebe85657b68bc7fe9b20124729690f9
sparkie11 <gautamwise.s@gmail.com>
2024-10-28 16:12:13 +0530

    n

--- src/components/works/Work.jsx
+++ src/components/works/Work.jsx
@@ -1,7 +1,30 @@
-import "./works.scss"
+import './works.scss'
 
 export default function Work() {
   return (
-    <div className="work" id="works"> Work</div>
+    <div className="works" id="works">
+      <div className="slider">
+        <div className="container">
+          <div className="item">
+            <div className="left">
+              <div className="leftContainer">
+                <div className="imgContainer">
+                  <img src="assets/bike2.png" alt="" />
+                </div>
+              <h2>title</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
+                voluptatum harum vel alias repellendus quos at porro dolor sequi
+                dignissimos ipsam eaque, ratione quidem molestiae iure, ab
+                maiores. Officiis, voluptatibus.
+              </p>
+              <span>Projects</span>
+              </div>
+            </div>
+            <div className="right"></div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
