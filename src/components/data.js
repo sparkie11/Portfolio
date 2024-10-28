c3d8d1352cec7e1d9592e334f0d03b836af7d987
sparkie11 <gautamwise.s@gmail.com>
2024-10-28 14:36:46 +0530

    n

---
+++ src/components/data.js
@@ -0,0 +1,32 @@
+export const featuredPortfolio = [
+  {
+    id: 1,
+    title: 'content kesar web design ',
+    img: 'https://gautamwise.com/_next/image?url=%2Fimages%2Fcapeclouds.webp&w=256&q=75',
+  },
+  {
+    id: 2,
+    title: 'content kesar web design ',
+    img: 'https://gautamwise.com/_next/image?url=%2Fimages%2Fcapeclouds.webp&w=256&q=75',
+  },
+  {
+    id: 3,
+    title: 'content kesar web design ',
+    img: 'https://gautamwise.com/_next/image?url=%2Fimages%2Fcapeclouds.webp&w=256&q=75',
+  },
+  {
+    id: 4,
+    title: 'content kesar web design ',
+    img: 'https://gautamwise.com/_next/image?url=%2Fimages%2Fcapeclouds.webp&w=256&q=75',
+  },
+  {
+    id: 5,
+    title: 'content kesar web design ',
+    img: 'https://gautamwise.com/_next/image?url=%2Fimages%2Fcapeclouds.webp&w=256&q=75',
+  },
+  {
+    id: 6,
+    title: 'content kesar web design ',
+    img: 'https://gautamwise.com/_next/image?url=%2Fimages%2Fcapeclouds.webp&w=256&q=75',
+  },
+]
