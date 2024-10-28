0cf92f45c21078f4f3d6685da6be97440c37bc1e
sparkie11 <gautamwise.s@gmail.com>
2024-10-28 14:36:39 +0530

    n

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,35 +1,42 @@
+import { useState } from 'react';
+import PortfolioList from '../portfolioList/PortfolioList'
 import './portfolio.scss'
 
 export default function Portfolio() {
+  const [selected, setSelected] = useState("featured");
+  const list = [
+    {
+      id: 'featured',
+      title: 'Featured',
+    },
+    {
+      id: 'webApp',
+      title: 'Web App',
+    },
+    {
+      id: 'mobileApp',
+      title: 'Mobile App',
+    },
+    {
+      id: 'designing',
+      title: 'Designing',
+    },
+    {
+      id: 'branding',
+      title: 'Branding',
+    },
+  ]
   return (
     <div className="portfolio" id="portfolio">
       <h1>Porfolio</h1>
       <ul>
-        <li>Featured</li>
-        <li>Web App</li>
-        <li>Mobile App</li>
-        <li>Designing</li>
-        <li>Branding</li>
+        {list.map((item, index) => (
+          <PortfolioList key={index} title={item.title} active={selected === item.id} setSelected={setSelected} id={item.id}/>
+        ))}
       </ul>
 
       <div className="container">
-        <div className="item">
-          <img src='https://gautamwise.com/_next/image?url=%2Fimages%2Fcapeclouds.webp&w=256&q=75' alt=''/>
-          <h3>Banking app</h3>
-        </div>
-        <div className="item">
-          <img src='https://gautamwise.com/_next/image?url=%2Fimages%2Fcapeclouds.webp&w=256&q=75' alt=''/>
-          <h3>Banking app</h3>
-        </div>        <div className="item">
-          <img src='https://gautamwise.com/_next/image?url=%2Fimages%2Fcapeclouds.webp&w=256&q=75' alt=''/>
-          <h3>Banking app</h3>
-        </div>        <div className="item">
-          <img src='https://gautamwise.com/_next/image?url=%2Fimages%2Fcapeclouds.webp&w=256&q=75' alt=''/>
-          <h3>Banking app</h3>
-        </div>        <div className="item">
-          <img src='https://gautamwise.com/_next/image?url=%2Fimages%2Fcapeclouds.webp&w=256&q=75' alt=''/>
-          <h3>Banking app</h3>
-        </div>
+    
       </div>
     </div>
   )
