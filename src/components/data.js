9e997b6eced271d94180753d2c6a42967a4f1f16
sparkie11 <gautamwise.s@gmail.com>
2024-10-28 14:46:13 +0530

    n

--- src/components/data.js
+++ src/components/data.js
@@ -30,3 +30,139 @@ export const featuredPortfolio = [
     img: 'https://gautamwise.com/_next/image?url=%2Fimages%2Fcapeclouds.webp&w=256&q=75',
   },
 ]
+
+export const webPortfolio = [
+    {
+      id: 1,
+      title: 'content kesar web design ',
+      img: 'https://gautamwise.com/_next/image?url=%2Fimages%2Fcapeclouds.webp&w=256&q=75',
+    },
+    {
+      id: 2,
+      title: 'content kesar web design ',
+      img: 'https://gautamwise.com/_next/image?url=%2Fimages%2Fcapeclouds.webp&w=256&q=75',
+    },
+    {
+      id: 3,
+      title: 'content kesar web design ',
+      img: 'https://gautamwise.com/_next/image?url=%2Fimages%2Fcapeclouds.webp&w=256&q=75',
+    },
+    {
+      id: 4,
+      title: 'content kesar web design ',
+      img: 'https://gautamwise.com/_next/image?url=%2Fimages%2Fcapeclouds.webp&w=256&q=75',
+    },
+    {
+      id: 5,
+      title: 'content kesar web design ',
+      img: 'https://gautamwise.com/_next/image?url=%2Fimages%2Fcapeclouds.webp&w=256&q=75',
+    },
+    {
+      id: 6,
+      title: 'content kesar web design ',
+      img: 'https://gautamwise.com/_next/image?url=%2Fimages%2Fcapeclouds.webp&w=256&q=75',
+    },
+  ]
+  
+
+  export const mobilePortfolio = [
+    {
+      id: 1,
+      title: 'content kesar web design ',
+      img: 'https://gautamwise.com/_next/image?url=%2Fimages%2Fcapeclouds.webp&w=256&q=75',
+    },
+    {
+      id: 2,
+      title: 'content kesar web design ',
+      img: 'https://gautamwise.com/_next/image?url=%2Fimages%2Fcapeclouds.webp&w=256&q=75',
+    },
+    {
+      id: 3,
+      title: 'content kesar web design ',
+      img: 'https://gautamwise.com/_next/image?url=%2Fimages%2Fcapeclouds.webp&w=256&q=75',
+    },
+    {
+      id: 4,
+      title: 'content kesar web design ',
+      img: 'https://gautamwise.com/_next/image?url=%2Fimages%2Fcapeclouds.webp&w=256&q=75',
+    },
+    {
+      id: 5,
+      title: 'content kesar web design ',
+      img: 'https://gautamwise.com/_next/image?url=%2Fimages%2Fcapeclouds.webp&w=256&q=75',
+    },
+    {
+      id: 6,
+      title: 'content kesar web design ',
+      img: 'https://gautamwise.com/_next/image?url=%2Fimages%2Fcapeclouds.webp&w=256&q=75',
+    },
+  ]
+  
+
+  export const designPortfolio = [
+    {
+      id: 1,
+      title: 'content kesar web design ',
+      img: 'https://gautamwise.com/_next/image?url=%2Fimages%2Fcapeclouds.webp&w=256&q=75',
+    },
+    {
+      id: 2,
+      title: 'content kesar web design ',
+      img: 'https://gautamwise.com/_next/image?url=%2Fimages%2Fcapeclouds.webp&w=256&q=75',
+    },
+    {
+      id: 3,
+      title: 'content kesar web design ',
+      img: 'https://gautamwise.com/_next/image?url=%2Fimages%2Fcapeclouds.webp&w=256&q=75',
+    },
+    {
+      id: 4,
+      title: 'content kesar web design ',
+      img: 'https://gautamwise.com/_next/image?url=%2Fimages%2Fcapeclouds.webp&w=256&q=75',
+    },
+    {
+      id: 5,
+      title: 'content kesar web design ',
+      img: 'https://gautamwise.com/_next/image?url=%2Fimages%2Fcapeclouds.webp&w=256&q=75',
+    },
+    {
+      id: 6,
+      title: 'content kesar web design ',
+      img: 'https://gautamwise.com/_next/image?url=%2Fimages%2Fcapeclouds.webp&w=256&q=75',
+    },
+  ]
+
+  export const contentPortfolio = [
+    {
+      id: 1,
+      title: 'content kesar web design ',
+      img: 'https://gautamwise.com/_next/image?url=%2Fimages%2Fcapeclouds.webp&w=256&q=75',
+    },
+    {
+      id: 2,
+      title: 'content kesar web design ',
+      img: 'https://gautamwise.com/_next/image?url=%2Fimages%2Fcapeclouds.webp&w=256&q=75',
+    },
+    {
+      id: 3,
+      title: 'content kesar web design ',
+      img: 'https://gautamwise.com/_next/image?url=%2Fimages%2Fcapeclouds.webp&w=256&q=75',
+    },
+    {
+      id: 4,
+      title: 'content kesar web design ',
+      img: 'https://gautamwise.com/_next/image?url=%2Fimages%2Fcapeclouds.webp&w=256&q=75',
+    },
+    {
+      id: 5,
+      title: 'content kesar web design ',
+      img: 'https://gautamwise.com/_next/image?url=%2Fimages%2Fcapeclouds.webp&w=256&q=75',
+    },
+    {
+      id: 6,
+      title: 'content kesar web design ',
+      img: 'https://gautamwise.com/_next/image?url=%2Fimages%2Fcapeclouds.webp&w=256&q=75',
+    },
+  ]
+  
+  
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,18 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react'
 import PortfolioList from '../portfolioList/PortfolioList'
 import './portfolio.scss'
+import {
+  webPortfolio,
+  mobilePortfolio,
+  contentPortfolio,
+  featuredPortfolio,
+  designPortfolio,
+} from '../data'
 
 export default function Portfolio() {
-  const [selected, setSelected] = useState("featured");
+  const [selected, setSelected] = useState('featured')
+  const [data, setData] = useState([])
+
   const list = [
     {
       id: 'featured',
@@ -26,17 +35,52 @@ export default function Portfolio() {
       title: 'Branding',
     },
   ]
+
+  useEffect(() => {
+    switch (selected) {
+      case 'featured':
+        setData(featuredPortfolio)
+        break
+      case 'webApp':
+        setData(webPortfolio)
+        break
+      case 'mobileApp':
+        setData(mobilePortfolio)
+        break
+      case 'designing':
+        setData(designPortfolio)
+        break
+      case 'branding':
+        setData(contentPortfolio)
+        break
+      default:
+        setData(featuredPortfolio)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selected])
+
   return (
     <div className="portfolio" id="portfolio">
       <h1>Porfolio</h1>
       <ul>
         {list.map((item, index) => (
-          <PortfolioList key={index} title={item.title} active={selected === item.id} setSelected={setSelected} id={item.id}/>
+          <PortfolioList
+            key={index}
+            title={item.title}
+            active={selected === item.id}
+            setSelected={setSelected}
+            id={item.id}
+          />
         ))}
       </ul>
 
       <div className="container">
-    
+        {data.map(d => (
+          <div className="item">
+            <img src={d.img} />
+            <h3>{d.title}</h3>
+          </div>
+        ))}
       </div>
     </div>
   )
