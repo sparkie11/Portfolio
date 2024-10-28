053d65dcc40196d2c023ab214f5e98a773b3aa37
sparkie11 <gautamwise.s@gmail.com>
2024-10-28 11:34:04 +0530

    n

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,22 @@
+import {  South } from "@mui/icons-material"
 import "./intro.scss"
 
 export default function Intro() {
   return (
-    <div className="intro" id="intro">Intro</div>
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/wise.jpeg" alt="" className="introImg" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There I'm </h2>
+          <h1>Gautam wise</h1>
+          <h3>Freelance <span></span></h3>
+        </div>
+        <a href="#portfolio"><South className="icon"/></a>
+      </div>
+    </div>
   )
 }
