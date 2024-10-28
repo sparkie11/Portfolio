b84e6c2023e62a6ec12fa853e7dd1aa96c3fb832
sparkie11 <gautamwise.s@gmail.com>
2024-10-28 12:51:21 +0530

    n

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,26 @@
-import {  South } from "@mui/icons-material"
-import "./intro.scss"
+import { South } from '@mui/icons-material'
+import './intro.scss'
+import { init } from 'ityped'
+import { useEffect, useRef } from 'react'
 
 export default function Intro() {
+  const textRef = useRef(null)
+
+  useEffect(() => {
+    if (textRef.current) {
+      init(textRef.current, {
+        showCursor: true,
+        backDelay: 1500,
+        backSpeed: 50, // Lower backSpeed to prevent repeated characters
+        startDelay: 300,
+        strings: ['Developer', 'Designer', 'Content Creator'],
+      })
+    }
+    // Clean-up function to avoid duplicate animations
+    return () => {
+      // textRef.current && init(textRef.current).reset();
+    }
+  }, [])
   return (
     <div className="intro" id="intro">
       <div className="left">
@@ -13,9 +32,13 @@ export default function Intro() {
         <div className="wrapper">
           <h2>Hi There I'm </h2>
           <h1>Gautam wise</h1>
-          <h3>Freelance <span>Designer</span></h3>
+          <h3>
+            Freelance <span ref={textRef}></span>{' '}
+          </h3>
         </div>
-        <a href="#portfolio"><South className="icon"/></a>
+        <a href="#portfolio">
+          <South className="icon" />
+        </a>
       </div>
     </div>
   )
