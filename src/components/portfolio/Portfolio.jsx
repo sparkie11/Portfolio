c2748b189d79cb532c46578e736176ff693f6a78
sparkie11 <gautamwise.s@gmail.com>
2024-10-28 12:59:48 +0530

    n

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,23 @@
-import "./portfolio.scss"
+import './portfolio.scss'
 
 export default function Portfolio() {
   return (
-    <div className="portfolio" id="portfolio">Portfolio</div>
+    <div className="portfolio" id="portfolio">
+      <h1>Porfolio</h1>
+      <ul>
+        <li>Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+        <li>Designing</li>
+        <li>Branding</li>
+      </ul>
+
+      <div className="container">
+        <div className="item">
+          <img src='https://gautamwise.com/_next/image?url=%2Fimages%2Fcapeclouds.webp&w=256&q=75' alt=''/>
+          <h3>Banking app</h3>
+        </div>
+      </div>
+    </div>
   )
 }
