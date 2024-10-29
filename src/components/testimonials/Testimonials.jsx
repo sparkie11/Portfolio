6aebd2bc48dbfa34883d97d1cd70ad1260e2d42f
sparkie11 <gautamwise.s@gmail.com>
2024-10-29 14:45:19 +0530

    n

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -6,7 +6,34 @@ export default function Testimonials() {
     <div className="testimonials" id="testimonials">
       <h1>Testimonials</h1>
       <div className="container">
+        <div className="card featured">
+          <div className="top">
+            {/* <img src="" alt="" /> */}
+            <West className='left'/>
+            <img src="assets/bik1.png" className='user' alt="" />
+            <img className='right' src="assets/bike2.png" alt="" />
+
+          </div>
+          <div className="center">Lorem ipsrem adipisci earum sequi suscipit ea natus voluptas.</div>
+          <div className="bottom">
+            <h3>Alex</h3>
+            <h4>CEO of Maven</h4>
+          </div>
+        </div>
         <div className="card">
+          <div className="top">
+            {/* <img src="" alt="" /> */}
+            <West className='left'/>
+            <img src="assets/bik1.png" className='user' alt="" />
+            <img className='right' src="assets/bike2.png" alt="" />
+
+          </div>
+          <div className="center">Lorem ipsrem adipisci earum sequi suscipit ea natus voluptas.</div>
+          <div className="bottom">
+            <h3>Alex</h3>
+            <h4>CEO of Maven</h4>
+          </div>
+        </div>        <div className="card featured">
           <div className="top">
             {/* <img src="" alt="" /> */}
             <West className='left'/>
