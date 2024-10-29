1fc96f2f52ebcf3b59e85f573454a0c78900094f
sparkie11 <gautamwise.s@gmail.com>
2024-10-29 14:31:51 +0530

    n

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,7 +1,26 @@
-import "./testimonials.scss"
+import { West } from '@mui/icons-material'
+import './testimonials.scss'
 
 export default function Testimonials() {
   return (
-    <div className="testimonials" id="testimonials"> Testimonials</div>
+    <div className="testimonials" id="testimonials">
+      <h1>Testimonials</h1>
+      <div className="container">
+        <div className="card">
+          <div className="top">
+            {/* <img src="" alt="" /> */}
+            <West className='left'/>
+            <img src="assets/bik1.png" className='user' alt="" />
+            <img className='right' src="assets/bike2.png" alt="" />
+
+          </div>
+          <div className="center">Lorem ipsrem adipisci earum sequi suscipit ea natus voluptas.</div>
+          <div className="bottom">
+            <h3>Alex</h3>
+            <h4>CEO of Maven</h4>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
