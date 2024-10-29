89340619862494e52f96aa1a1f93ea5fc3c4fa71
sparkie11 <gautamwise.s@gmail.com>
2024-10-29 16:21:46 +0530

    n

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,29 @@
+import { useState } from "react"
 import "./contact.scss"
 
 export default function Contact() {
+
+  const [message, setMessage] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("Form submitted")
+    setMessage(true)
+  }
   return (
-    <div className="contact" id="contact" >Contact</div>
+    <div className="contact" id="contact" >
+      <div className="left">
+        <img src="assets/profile.png" alt="" />
+      </div>
+      <div className="right">
+        <h2>Contact.</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Email" />
+          <textarea name="" placeholder="Message" id=""></textarea>
+          <button type="submit">Send</button>
+          {message && <span>Message sent successfully!</span>}
+        </form>
+      </div>
+    </div>
   )
 }
